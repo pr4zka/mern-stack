6c1fb53d3728bd40e22a93553bcfd1028484375a
pr4zka <92057245+pr4zka@users.noreply.github.com>
2022-10-25 22:43:32 -0300

    hola

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,12 +7,14 @@ export function HomePage() {
     const { posts } = usePost()
 
     const renderMain = () => {
-        if (posts.length === 0) return (
-            <div className='flex flex-col justify-center items-center text-white text-xl'>
-                <VscEmptyWindow className='w-20 h-20 text-white' />
-                <h1>Here are no posts</h1>
-            </div>
-        )
+        if (posts.length === 0)
+            return (
+                <div className='flex flex-col justify-center items-center text-white text-xl'>
+                    <VscEmptyWindow className='w-20 h-20 text-white' />
+                    <h1>Here are no posts</h1>
+                </div>
+            )
+
         return (
             <div className='grid grid-cols-3 gap-2'>
                 {posts.map(post => (
@@ -22,12 +24,20 @@ export function HomePage() {
         )
     }
     return (
-        <div className='text-white'>
-            <header className='flex justify-between py-4'>
-                <h1 className='text-2xl text-gray-300 font-bold'>Post: {posts.length}</h1>
-                <Link to={'/new'} className="text-white text-2xl px-3 py-2 bg-indigo-500 hover:bg-indigo-600">Add Post</Link>
+        <main>
+            <header className="flex justify-between items-center my-4">
+                <h1 className="text-2xl text-gray-300 font-bold">
+                    Posts ({posts.length})
+                </h1>
+                <Link
+                    to="/new"
+                    className="bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-500"
+                >
+                    Create Post
+                </Link>
             </header>
+
             {renderMain()}
-        </div>
+        </main>
     )
 }
